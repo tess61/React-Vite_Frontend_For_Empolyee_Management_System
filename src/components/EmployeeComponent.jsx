a11fb8063cacbb4b497e8016f87769e4ddab8ae4
tess61 <hopemelkamu28@gmail.com>
2024-01-20 11:56:37 +0300

    Add button fixed

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { createEmployee } from '../services/EmployeeService';
+import { useNavigate } from 'react-router-dom';
 
 const EmployeeComponent = () => {
 
@@ -6,12 +8,18 @@ const EmployeeComponent = () => {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
 
+    const navigator = useNavigate()
+
     function saveEmployee (e) {
         e.preventDefault()
 
         const employee = {firstName,lastName, email};
         console.log(employee)
 
+        createEmployee(employee).then((response) => {
+            console.log(response.data)
+            navigator('/employees')
+        })
     }
   return (
     <div className='container'> <br /><br />
