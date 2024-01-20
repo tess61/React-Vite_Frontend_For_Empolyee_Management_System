695e993d02331368e94af289512856b56d12eb6b
tess61 <hopemelkamu28@gmail.com>
2024-01-20 10:12:17 +0300

    configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import HeaderComponent from './components/HeaderComponent'
 import LIstEmployeeComponent from './components/LIstEmployeeComponent'
+import FooterComponent from './components/FooterComponent'
+import { BrowserRouter , Route, Routes} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <LIstEmployeeComponent />
+    <>
+    <BrowserRouter>
+    <HeaderComponent />
+    <Routes>
+      {/* // http://localhost:3000 */}
+      <Route path='/' element = { <LIstEmployeeComponent /> }></Route>
+      {/* // http://localhost:3000/employees */}
+      <Route path='/employees' element = { < LIstEmployeeComponent />}></Route>
+    </Routes>
+    <FooterComponent />
+    </BrowserRouter>
+    
+    </>
+    
   )
 }
 
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All Rights Reserved 2024 by ### ems </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
